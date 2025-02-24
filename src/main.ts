d9cf8e5c45db0dd420dfeb34488705fba1267432
pedro-donoso <pedro.donoso.r@usach.cl>
2025-02-24 00:15:08 -0300

    agrego comentarios

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// Main es el punto de entrada de la aplicacion, donde se configuran los modulos, los controladores y los proveedores
 async function bootstrap() {
+  // Creo la aplicacion NestJS
   const app = await NestFactory.create(AppModule);
+
+  // Habilito CORS
   app.enableCors();
+
+  // Inicio la aplicacion
   await app.listen(process.env.PORT ?? 3000);
 }
+
+// Llamada a la funcion bootstrap
 bootstrap();
