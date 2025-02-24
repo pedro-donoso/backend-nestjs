11e66e9aa8614d7a0bbe3fac93f28bf911a3266c
pedro-donoso <pedro.donoso.r@usach.cl>
2025-02-23 23:41:57 -0300

    agrego comentarios

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { UserController } from './controllers/user.controller';
 import { UserService } from './services/user.service';
 
+// AppModule es el modulo principal de la aplicacion, donde se configuran los controladores, los proveedores y los imports
 @Module({
+  // Los imports son otros modulos que se necesitan para que este modulo funcione
   imports: [],
+  // Los controllers son los controladores de la aplicacion, donde se manejan las peticiones HTTP
   controllers: [UserController],
+  // Los providers son los proveedores de la aplicacion, como los servicios
   providers: [UserService],
 })
 export class AppModule {}
